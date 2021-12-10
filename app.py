35fe849776fe7d6f0b5df4fe3c9044508a44a5e0
Stefany Lima <limastefany900@gmail.com>
2021-12-10 15:27:00 -0600

    start flask app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+#Dependencies
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import datetime as dt
+
+
+
+
+#Database setup
+
+
+
+
+#Flask Setup
+
+
+
+
+#Flask routes
